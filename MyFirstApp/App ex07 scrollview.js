180a2bfd05c3d8270f4d397943bc17e63eb63443
walafi <walafiferreira@trixlog.com>
2019-09-20 13:39:13 -0300

    Implementando listagens

---
+++ MyFirstApp/App ex07 scrollview.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { ScrollView, StyleSheet, Text, Image, View } from 'react-native'
+
+
+
+export default class MyFirstApp extends Component {
+
+	render() {
+		return (
+			<ScrollView>
+				<View>
+					<Text style={{fontSize: 96}}>Scroll me plz</Text>
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Text style={{fontSize: 96}}>Text 02</Text>
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Text style={{fontSize: 96}}>Text 02</Text>
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Text style={{fontSize: 96}}>Text 02</Text>
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+					<Image source={{uri: "https://facebook.github.io/react-native/img/tiny_logo.png", width: 64, height: 64}} />
+				</View>
+			</ScrollView>
+		)
+	}
+}
+
+
+
+const styles = StyleSheet.create({
+
+})
--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -1,75 +1,93 @@
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View, FlatList, SectionList } from 'react-native'
 
 
 
 export default class MyFirstApp extends Component {
 
-	onPressButton(){
-		alert('You tapped the botton')
-	}
-
-	onLongPressButton(){
-		alert('You long pressed the button')
-	}
-
 	render() {
 		return (
-			<View style={styles.container}>
-				<TouchableOpacity onPress={this.onPressButton} underlayColor="white">
-					<View style={styles.button}>
-						<Text style={styles.buttonText}>TouchableHighlight</Text>
-					</View>
-				</TouchableOpacity>
-
-				<TouchableOpacity onPress={this.onPressButton}>
-					<View style={styles.button}>
-						<Text style={styles.buttonText}>TouchableOpacity</Text>
-					</View>
-				</TouchableOpacity>
-
-
-				<TouchableNativeFeedback 
-					onPress={this.onPressButton}
-					background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
-					<View style={styles.button}>
-						<Text style={styles.buttonText}>
-							TouchableNativeFeedback {Platform.OS !== 'android' ? '(Android only)' : ''}
-						</Text>
-					</View>
-				</TouchableNativeFeedback>
-
-				<TouchableWithoutFeedback onPress={this.onPressButton}>
-					<View style={styles.button}>
-						<Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-					</View>
-				</TouchableWithoutFeedback>
-				<TouchableHighlight onPress={this.onPressButton} onLongPress={this.onLongPressButton} underlayColor="white">
-					<View style={styles.button}>
-						<Text style={styles.buttonText}>Touchable with Long Press</Text>
-					</View>	
-				</TouchableHighlight>
-			</View>
+			<ScrollView>
+				<View style={styles.container}>
+					<SectionList
+						sections={[
+							{title: '0', data: ['alan', 'ronaldo', 'fernando']},
+							{title: '1', data: ['roger', 'luan', 'lucar', 'julio', 'walafi']}
+						]}
+						renderItem= {({item}) => <Text style={styles.item}>{item}</Text>}
+						renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+						keyExtractor={({item, index}) => index}
+					/>
+				</View>
+			</ScrollView>
 		)
 	}
 }
 
-
-
 const styles = StyleSheet.create({
 	container: {
-		paddingTop: 60,
-		alignItems: "center"
+		flex: 1,
+		paddingTop: 22
 	},
-	button:{
-		marginBottom: 30,
-		width: 200,
-		alignItems: "center",
-		backgroundColor: "#2196F3"
-	},	
-	buttonText: {
-		textAlign: "center",
-		padding: 20,
-		color: "white"
+	sectionHeader: {
+		paddingTop: 2,
+		 paddingLeft: 10,
+		 paddingRight: 10,
+		 paddingBottom: 2,
+		 fontSize: 14,
+		 fontWeight: "bold",
+		 backgroundColor: 'rgba(247,247,247,1.0)',
+	},
+	item: {
+		padding: 10,
+		fontSize: 18,
+		height: 44
 	}
 })
+
+
+// import React, { Component } from 'react'
+// import { ScrollView, StyleSheet, Text, View, FlatList } from 'react-native'
+
+
+
+// export default class MyFirstApp extends Component {
+
+// 	render() {
+// 		return (
+// 			<ScrollView>
+// 				<View style={styles.container}>
+// 					<FlatList 
+// 						data={[
+// 							{key: 'Devin'},
+// 							{key: 'Dan'},
+// 							{key: 'Dominic'},
+// 							{key: 'Jackson'},
+// 							{key: 'James'},
+// 							{key: 'Joel'},
+// 							{key: 'John'},
+// 							{key: 'Jillian'},
+// 							{key: 'Jimmy'},
+// 							{key: 'Julie'}
+// 						  ]}
+// 						renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+// 					/>
+// 				</View>
+// 			</ScrollView>
+// 		)
+// 	}
+// }
+
+
+
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 		paddingTop: 22
+// 	},
+// 	item: {
+// 		padding: 10,
+// 		fontSize: 18,
+// 		height: 44
+// 	}
+// })
