5e56b056da26147983528bf0d7c3def106c99df1
Walafi02 <walafif@@yahoo.com>
2019-09-29 20:27:47 -0300

    incluindo hooks no projeto

--- calculadora/.eslintrc.js
+++ calculadora/.eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    extends: '@react-native-community',
+    plugins: [
+        // ...
+        'react-hooks',
+    ],
+    rules: {
+        'no-eval': 0,
+        'no-alert': 0,
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    },
 };
